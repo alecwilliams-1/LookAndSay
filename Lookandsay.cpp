8b2048510537ed297362576d129bd3cc66db59db
Alec Williams <31707327+alecwilliams-1@users.noreply.github.com>
2018-11-20 01:39:21 -0600

    includes and some formatting

--- Lookandsay.cpp
+++ Lookandsay.cpp
@@ -1,15 +1,19 @@
+#include <iostream>
+#include <string>
+
 /*chars to store
-next char in the stirng,
+next char in the string,
 the character being looked for,
 and the number of characters of lookFor that have been seen so far*/
 char next, LookFor, count;
 
 /* the string that is being analyzed and
 the string that is being constructed from the one being parsed*/
-char* LookSayString, newString;
+char* LookSayString;
+char* newString;
 
 //initialize the string to 1 in ASCII
-&LookSayString = 49;
+*LookSayString = 49;
 
 //debug statement because I'm not exactly a pro.
 cout << LookSayString;
