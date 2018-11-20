d2625e0defdc13769c2da6c95d9b2ac2c06416af
Alec Williams <31707327+alecwilliams-1@users.noreply.github.com>
2018-11-20 01:11:30 -0600

    Update Lookandsay.cpp

--- Lookandsay.cpp
+++ Lookandsay.cpp
@@ -1,1 +1,15 @@
+/*chars to store
+next char in the stirng,
+the character being looked for,
+and the number of characters of lookFor that have been seen so far*/
+char next, LookFor, count;
 
+/* the string that is being analyzed and
+the string that is being constructed from the one being parsed*/
+char* LookSayString, newString;
+
+//initialize the string to 1 in ASCII
+&LookSayString = 49;
+
+//debug statement because I'm not exactly a pro.
+cout << LookSayString;
